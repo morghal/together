defe826d3cd675bfd4af8df64a781b58053d0358
Morgan <morganhalin@gmail.com>
2023-03-28 10:05:40 +0200

    Ajout du template edit

--- resources/js/Pages/Edit.vue
+++ resources/js/Pages/Edit.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+    import navbar from '../Components/FooterNav.vue'
+</script>
 <template>
     <div class="container bg-slate-50">
     <header class="">
@@ -35,21 +37,27 @@
                         </svg>
                     </div>
                 </label>
-                <img class="shadow-xl shadow-pewter-blue" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.paysdevire-normandie-tourisme.fr%2Fwp-content%2Fuploads%2F2020%2F03%2F2-randonneepedestre-1024x686.jpg&f=1&nofb=1&ipt=9813148e396420559e89d2b48fbaafe3bc06fc575c03cf50c7b8b5c525551bec&ipo=images" alt="">
+                <img class="shadow-lg shadow-pewter-blue" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.paysdevire-normandie-tourisme.fr%2Fwp-content%2Fuploads%2F2020%2F03%2F2-randonneepedestre-1024x686.jpg&f=1&nofb=1&ipt=9813148e396420559e89d2b48fbaafe3bc06fc575c03cf50c7b8b5c525551bec&ipo=images" alt="">
             </div>
-            <label for="titre" class="text-slate-900 font-semibold">Titre</label>
+            
            
            <!--TITRE-->
+           <label for="titre" class="text-slate-900 font-bold text-sm">Titre</label>
             <div class="relative mb-6">
-                <input type="text" id="titre" name="titre" class="rounded-full bg-crystal px-4 py-1 w-full" value="Randonnée charmante au bord d'un lac">
+                <input type="text" id="titre" name="titre" class="rounded-lg bg-crystal pr-4 py-1 w-full" value="Randonnée charmante au bord d'un lac">
                 <svg fill="none" class="w-4 h-4 absolute right-3 top-2" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
                   </svg>
             </div>
             
             <!--CATEGORIES-->
-            <fieldset class="flex flex-wrap">
-                
+            <fieldset class="flex flex-wrap mb-6">
+                <legend class="text-slate-900 font-bold mb-4 text-sm">
+                    <svg fill="none" stroke="currentColor" class="inline h-6 w-6 mr-3" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"></path>
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z"></path>
+                    </svg>Categories
+                </legend>
                 <div class="mb-5">
                     <input type="checkbox" name="autres" id="autres" checked class="hidden peer">
                     <label for="autres" class="bg-crystal mr-2 text-slate-600 shadow-sm px-3 py-2 rounded-lg peer-checked:bg-jellybeanblue peer-checked:text-slate-50 shadow-pewter-blue">
@@ -119,13 +127,109 @@
                     Rencontre
                     </label>
                 </div>  
-
-                
             </fieldset>
+
+            <!--DATE_ACTIVITE-->
+            <label for="dateActivite" class="text-slate-900 font-bold mb-4 text-sm">Date de l'activité</label>
+            <div class="relative mb-6">
+                <input type="date" class="rounded-lg bg-crystal relative pr-4 py-1 w-full" id="dateActivite" name="dateActivite" value="2023-10-27" min="2023-10-27" max="2024-10-27">
+                <svg fill="none" class="w-4 h-4 absolute right-3 top-2" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
+                  </svg>
+            </div>
+
+            <!--HEURE_ACTIVITE-->
+            <label for="heureActivite" class="text-slate-900 font-bold mb-4 text-sm">Heure de l'activité</label>
+            <div class="relative mb-6">
+                <input type="time" class="rounded-lg bg-crystal relative pr-4 py-1 w-full" id="heureActivite" name="heureActivite" value="12:13">
+                <svg fill="none" class="w-4 h-4 absolute right-3 top-2" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
+                  </svg>
+            </div>
+
+            <!--DUREE_ACTIVITE-->
+            <label for="dureeActivite" class="text-slate-900 font-bold mb-4 text-sm">Durée de l'activité</label>
+            <div class="relative mb-6">
+                <input type="time" class="rounded-lg bg-crystal relative pr-4 py-1 w-full" id="dureeActivite" name="dureeActivite" value="03:00" min="01:00" max="12:00">
+                <svg fill="none" class="w-4 h-4 absolute right-3 top-2" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
+                  </svg>
+            </div>
+
+            <!--NOMBRE_PARTICIPANTS-->  
+            <label for="nbrParticipants" class="text-slate-900 font-bold mb-4 text-sm">Nombre de participants</label>
+            <div class="relative mb-6">
+                <input type="number" class="rounded-lg bg-crystal relative pr-4 py-1 w-full" id="nbrParticipants" name="nbrParticipants" value="1" min="1" max="25">
+                <svg fill="none" class="w-4 h-4 absolute right-3 top-2" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
+                  </svg>
+            </div>
+
+            <!--ADRESSE-->
+            <label for="adresse" class="text-slate-900 font-bold mb-4 text-sm">Adresse</label>
+            <div class="relative mb-6">
+                <input type="text" class="rounded-lg bg-crystal relative px-r py-1 w-full" id="adresse" name="adresse" value="Rue de la Belle Garde, 27">
+                <svg fill="none" class="w-4 h-4 absolute right-3 top-2" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
+                  </svg>
+            </div>
+
+            <!--VILLE-->
+            <label for="ville" class="text-slate-900 font-bold mb-4 text-sm">Ville</label>
+            <div class="relative mb-6">
+                <input type="text" class="rounded-lg bg-crystal relative pr-4 py-1 w-full" id="ville" name="ville" value="Saint-Germain-de-Tallevende-la-Lande...">
+                <svg fill="none" class="w-4 h-4 absolute right-3 top-2" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
+                  </svg>
+            </div>
+
+            <!--PAYS-->
+            <label for="pays" class="text-slate-900 font-bold mb-4 text-sm">Pays</label>
+            <div class="relative mb-6">
+                <input type="text" class="rounded-lg bg-crystal relative pr-4 py-1 w-full" id="pays" name="pays" value="Belgique">
+                <svg fill="none" class="w-4 h-4 absolute right-3 top-2" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
+                  </svg>
+            </div>
+
+            <!--DESCRIPTION ACTIVITE-->
+            <label for="description" class="text-slate-900 font-bold mb-4 text-sm">Description de l'activité</label>
+            <div class="relative mb-6">
+                <textarea maxlength="250" class="rounded-lg bg-crystal relative h-52 pr-10 py-1 w-full" id="description" name="description">Cette randonnée vous fera perdre la tête ! De cours d'eau enchanteurs aux sommets ravissants, vous serez revigorés par cette balade d'un charme authentique
+                </textarea>
+                <svg fill="none" class="w-4 h-4 absolute right-3 top-2" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
+                  </svg>
+            </div>
         </form>
     </main>
+    <footer>
+        <navbar></navbar>
+    </footer>
     </div>
 </template>
 <style>
-   
+   input[type="date"]::-webkit-calendar-picker-indicator {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: auto;
+    height: auto;
+    color: transparent;
+    background: transparent;
+}
+
+    input[type="time"]::-webkit-calendar-picker-indicator {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: auto;
+    height: auto;
+    color: transparent;
+    background: transparent;
+}
 </style>
--- resources/js/Pages/Show.vue
+++ resources/js/Pages/Show.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+  import navbar from '../Components/FooterNav.vue'
+</script>
 <template>
     <article class="bg-gradient-to-b from-moonstone to-[#539ABB]">
     <header class="relative">
@@ -88,31 +90,8 @@
         </section>
     </main>
     <footer>
-            <nav class="w-full bg-slate-50 py-3 px-8">
-              <ul class="flex text-slate-800 text-center justify-between">
-                <li><a href="/home"><svg fill="none" class="h-6 w-6" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
-                      </svg>
-                    </a></li>
-
-                <li><a href="/profile"><svg fill="none" class="h-6 w-6" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path>
-                      </svg>
-                    </a></li>
-
-                <li><a href="/favoris"><svg fill="none" class="h-6 w-6" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"></path>
-                      </svg>
-                    </a></li>
-
-                <li><a href="/paramètres"><svg fill="none" class="h-6 w-6" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"></path>
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                      </svg>
-                    </a></li>
-              </ul>
-            </nav>
-          </footer>
+      <navbar></navbar>
+    </footer>
 </article>
 </template>
 <style>
